ddcc8579401d49365e413e861457947f73f0e93b
Lonkmaster <link2040abcd@gmail.com>
2024-07-27 16:59:08 -0500

    Tic-Tac-Toe works in the console

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
-let currentPlayer;
+const O_WIN_VALUE = 1;
+const TIE_VALUE = 2
+const X_WIN_VALUE = 3;
+const NOTHING_VALUE = 0;
 
 const playerOne = {
     name: "playerOne",
@@ -11,8 +13,10 @@ const playerTwo = {
     marker: "O"
 }
 
-let board = ["-","-","-","-","-","-","-","-","-"]
+let currentPlayer = playerOne
 
+
+let board = ["-","-","-","-","-","-","-","-","-"]
 //print Board
 function printBoard() {
     console.log(`${board[0]} ${board[1]} ${board[2]}`)
@@ -32,7 +36,7 @@ function selection(marker, num) {
 //check for winner
 
 function checkWinningCombo (marker) {
-    
+    let emptySpace = 0;
     let winningCombo = [
         //winning row
         [0, 1, 2],
@@ -53,212 +57,92 @@ function checkWinningCombo (marker) {
         let currentCombination = winningCombo[i] // [0, 1, 2]
         if(board[currentCombination[0]] == marker && board[currentCombination[1]] == marker && board[currentCombination[2]] == marker){
             console.log("You win!")
+            if(marker == "X"){
+                return X_WIN_VALUE;
+            }
+            return O_WIN_VALUE
         }
-        
     }
+
+    for(let i = 0; i < board.length; i++) {
+        let availableSpots = board[i]
+
+        if(availableSpots == "-") {
+            emptySpace++ 
+        }
+    }
+    if(emptySpace == 0) {
+        console.log("It's a tie")
+        return TIE_VALUE
+    }
+    return NOTHING_VALUE
 }
 
 // take turn
 
 function takeTurn() {
 
-    currentPlayer = playerOne
+    function selection(marker, num) {
+        if(num > 9) {
+            return
+        }
+        let x = num
+        board[x] = marker
+    }
+    
+    let position;
 
     if(currentPlayer == playerOne) {
-
-        let position;
 
         while (true) {
             position = prompt("Pick a number 0 - 8")
             if(position < 9 && position >= 0 && board[position] == "-") {
+                selection(playerOne.marker, position)
                 break
             }
             position = "not a valid Number!"
         }
 
         currentPlayer = playerTwo;
-
-        console.log("nigga");
+        return checkWinningCombo(playerOne.marker)
         
     } else {
 
         let computerChoice = Math.floor(Math.random() * 9);
-        
+
         while (true) {
             if(board[computerChoice] == "-") {
-                position == computerChoice
+                position = computerChoice
+                selection(playerTwo.marker, position)
                 break
             }
             computerChoice = Math.floor(Math.random() * 9);
         }
-
+        currentPlayer = playerOne
+        return checkWinningCombo(playerTwo.marker)
     }
 
 }
 
-takeTurn()
 
-selection(playerOne.marker, 3)
-checkWinningCombo(playerOne.marker)
-printBoard()
+function playGame() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const playerOne = {
-    name: "playerOne",
-    marker: "X"
-}
-
-const playerTwo = {
-    name: "PlayerTwo",
-    marker: "O"
-}
-
-
-function printName(selector) {
-    console.log(selector.name);
-}
-
-
-
-printName(playerTwo)
-
-
-function createGrid() {
-    let row1 = ["", "", ""];
-    let row2 = ["", "", ""];
-    let row3 = ["", "", ""];
-    let boardList = [row1, row2, row3];
-    return boardList;
-};
-
-
-
-function selection(marker, num) {
-
-    if(num > 9) {
-        return
-    }
-    let x = (num)%3
-    let y = Math.floor((num)/3)
-
-    board[y][x] = marker
-
-}
-
-
-function winCheck() {
-    const winCombinations = [
-        //row 
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        // column
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        //diagonal
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-
-   
-}
-
-let board = createGrid()
-
-function checkCombination(combination) {
-    //combination = [0, 1, 2]
-    let previousCell = null
-
-    for(let i = 0; i < combination.length; i++) {
-        let currentIndex = combination[i]
-        
-        let x = (currentIndex)%3
-        let y = Math.floor((currentIndex)/3)
-        console.log(board[x][y])
-        console.log(currentIndex)
-        if(previousCell == null) {
-            previousCell = board[x][y]
-            continue
-        }
-        if(previousCell != board[x][y]) {
-            console.log(previousCell)
-            return false
+    while(true) {
+        printBoard()
+        let result = takeTurn()
+        switch (result) {
+            case X_WIN_VALUE:
+                console.log("X won the game!");
+                return;
+            case O_WIN_VALUE:
+                console.log("O won the game!");
+                return;
+            case TIE_VALUE:
+                console.log("The game was a tie!");
+                return;
         }
     }
-    if(previousCell != "") {
-        return true
-    }
-
-    return false;
 }
 
-
-selection(playerOne.marker, 0)
-
-console.log(checkCombination([0, 1, 2]))
-
-
-
-console.log(board)
-
-
-
-const board = (function() {
-    let boardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let playerNum; 
-
-    function print() {
-        let text = "";
-        for (let i = 0; i < boardNumbers.length; i++) {
-            text += boardNumbers[i];
-            if (text.length == 3) {
-                console.log(text);
-                text = "";
-            }
-        }
-    }
-
-    function sayPickSpot() {
-        console.log("pick a spot based on the number");
-        playerNum = prompt("pick a number")
-
-    }
-
-    function currentPlayer() {
-
-    }
-
-    return {
-        print,
-        sayPickSpot,
-        currentPlayer,
-    }
-})();
-
-
-
-board.print()
-board.sayPickSpot()*/
+playGame()
 
