799db94a61f772de6d9ca44bdb5278e4105bd546
Lonkmaster <link2040abcd@gmail.com>
2024-08-06 20:37:22 -0500

    the code is no modular

--- script.js
+++ script.js
@@ -1,4 +1,161 @@
-const O_WIN_VALUE = 1;
+
+
+
+
+
+
+
+
+
+
+//player maker
+function playerMaker(name, marker) {
+    this.name = name;
+    this.marker = marker
+}
+
+// game board
+const gameboard = (function (playerOne, playerTwo) {
+    let currentPlayer = playerOne
+    const O_WIN_VALUE = 1;
+    const TIE_VALUE = 2
+    const X_WIN_VALUE = 3;
+    const NOTHING_VALUE = 0;
+
+    let board = ["-","-","-","-","-","-","-","-","-"]
+    function printBoard() {
+        console.log(`${board[0]} ${board[1]} ${board[2]}`)
+        console.log(`${board[3]} ${board[4]} ${board[5]}`)
+        console.log(`${board[6]} ${board[7]} ${board[8]}`)
+    };
+
+    const checkForWinner = function (marker){
+        let emptySpace = 0;
+        let winningCombo = [
+            //winning row
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            //winning column
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            //winning diagonal
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        
+        for(let i = 0; i < winningCombo.length; i++) {
+
+            let currentCombination = winningCombo[i] // [0, 1, 2]
+            if(board[currentCombination[0]] == marker && board[currentCombination[1]] == marker && board[currentCombination[2]] == marker){
+                if(marker == "X"){
+                    console.log("You win!")
+                    return X_WIN_VALUE;
+                }
+                console.log("You lose!")
+                return O_WIN_VALUE 
+            }
+        }
+        
+        for(let i = 0; i < board.length; i++) {
+            let availableSpots = board[i]
+    
+            if(availableSpots == "-") {
+                emptySpace++ 
+            }
+        }
+        if(emptySpace == 0) {
+            console.log("It's a tie")
+            return TIE_VALUE
+        }
+        return NOTHING_VALUE
+    }
+    
+    // take turn
+    function takeTurn() {
+
+        function selection(marker, num) {
+            if(num > 9) {
+                return
+            }
+            let x = num
+            board[x] = marker
+        }
+        
+        let position;
+
+        if(currentPlayer == playerOne) {
+
+            while (true) {
+                position = prompt("Pick a number 0 - 8")
+                if(position < 9 && position >= 0 && board[position] == "-") {
+                    selection(playerOne.marker, position)
+                    break
+                }
+                position = "not a valid Number!"
+            }
+
+            currentPlayer = playerTwo;
+            return checkForWinner(playerOne.marker)
+            
+        } else {
+
+            let computerChoice = Math.floor(Math.random() * 9);
+
+            while (true) {
+                if(board[computerChoice] == "-") {
+                    position = computerChoice
+                    selection(playerTwo.marker, position)
+                    break
+                }
+                computerChoice = Math.floor(Math.random() * 9);
+            }
+            currentPlayer = playerOne
+            return checkForWinner(playerTwo.marker)
+        }
+
+    }
+    function playGame() {
+
+        while(true) {
+            let result = takeTurn()
+            printBoard()
+            switch (result) {
+                case X_WIN_VALUE:
+                    console.log("X won the game!");
+                    return;
+                case O_WIN_VALUE:
+                    console.log("O won the game!");
+                    return;
+                case TIE_VALUE:
+                    console.log("The game was a tie!");
+                    return;
+            }
+        }
+    }
+
+    return {
+        playGame,
+    }
+})(new playerMaker("playerOne", "X"), new playerMaker("playerTwo", "O"));
+
+//gameboard.playGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const O_WIN_VALUE = 1;
 const TIE_VALUE = 2
 const X_WIN_VALUE = 3;
 const NOTHING_VALUE = 0;
@@ -145,4 +302,5 @@ function playGame() {
 }
 
 playGame()
+*/ 
 
