b5e8da5714781c7fc60e72a9637ea4d3c3596959
Lonkmaster <link2040abcd@gmail.com>
2024-08-11 02:15:14 -0500

    updated the color scheme on the website and add shadow, also made the restart button restart the board.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-
-
-
-
-
-
-
-
-
 //player maker
 function playerMaker(name, marker) {
     this.name = name;
@@ -15,14 +6,43 @@ function playerMaker(name, marker) {
 }
 
 // game board
-const gameboard = (function (playerOne, playerTwo) {
+const gameboard = (function (playerOne, playerTwo, dom) {
     let currentPlayer = playerOne
+
+    let turnDisplay = dom.querySelector(".turnDisplay")
+    let playerScore = dom.querySelector(".playerScore")
+    let computerScore = dom.querySelector(".computerScore")
+    let cells = dom.querySelectorAll(".cell")
+    let restartBtn = dom.querySelector(".restart")
+
+    for(let i = 0; i < cells.length; i++){
+        let cellsIndex = cells[i].dataset.index;
+        cells[i].addEventListener("click", () => { 
+            console.log(gameOver)
+            if(gameOver == true){
+                return
+            }
+            console.log(cellsIndex)
+            let turnResult = takeTurn(+cellsIndex);
+            if( turnResult != NOTHING_VALUE || turnResult == GAME_OVER_VALUE) {
+                return 
+            }
+            takeTurn()
+        })
+    }
+
+    let playerIncrement = 0
+    let computerIncrement = 0
+
     const O_WIN_VALUE = 1;
     const TIE_VALUE = 2
     const X_WIN_VALUE = 3;
     const NOTHING_VALUE = 0;
+    const GAME_OVER_VALUE = 4;
+    let gameOver = false
 
     let board = ["-","-","-","-","-","-","-","-","-"]
+
     function printBoard() {
         console.log(`${board[0]} ${board[1]} ${board[2]}`)
         console.log(`${board[3]} ${board[4]} ${board[5]}`)
@@ -50,9 +70,17 @@ const gameboard = (function (playerOne, playerTwo) {
             let currentCombination = winningCombo[i] // [0, 1, 2]
             if(board[currentCombination[0]] == marker && board[currentCombination[1]] == marker && board[currentCombination[2]] == marker){
                 if(marker == "X"){
+                    playerIncrement += 1
+                    playerScore.textContent = "Score: "+ playerIncrement
+                    turnDisplay.textContent = "X Won!"
                     console.log("You win!")
+                    gameOver = true
                     return X_WIN_VALUE;
                 }
+                gameOver = true
+                computerIncrement += 1
+                computerScore.textContent = "Score: "+ computerIncrement
+                turnDisplay.textContent = "O Won!"
                 console.log("You lose!")
                 return O_WIN_VALUE 
             }
@@ -66,6 +94,8 @@ const gameboard = (function (playerOne, playerTwo) {
             }
         }
         if(emptySpace == 0) {
+            gameOver = true
+            turnDisplay.textContent = "It's a tie!"
             console.log("It's a tie")
             return TIE_VALUE
         }
@@ -73,8 +103,7 @@ const gameboard = (function (playerOne, playerTwo) {
     }
     
     // take turn
-    function takeTurn() {
-
+    function takeTurn(cellsnNumber) {
         function selection(marker, num) {
             if(num > 9) {
                 return
@@ -86,14 +115,14 @@ const gameboard = (function (playerOne, playerTwo) {
         let position;
 
         if(currentPlayer == playerOne) {
+            
+            if(cellsnNumber < 9 && cellsnNumber >= 0 && board[cellsnNumber] == "-") {
+                cells[cellsnNumber].textContent = "X"
+                selection(playerOne.marker, cellsnNumber)
 
-            while (true) {
-                position = prompt("Pick a number 0 - 8")
-                if(position < 9 && position >= 0 && board[position] == "-") {
-                    selection(playerOne.marker, position)
-                    break
-                }
-                position = "not a valid Number!"
+            }
+            else {
+                return GAME_OVER_VALUE;
             }
 
             currentPlayer = playerTwo;
@@ -103,10 +132,13 @@ const gameboard = (function (playerOne, playerTwo) {
 
             let computerChoice = Math.floor(Math.random() * 9);
 
+
             while (true) {
                 if(board[computerChoice] == "-") {
                     position = computerChoice
+                    cells[position].textContent = "O"
                     selection(playerTwo.marker, position)
+
                     break
                 }
                 computerChoice = Math.floor(Math.random() * 9);
@@ -116,6 +148,7 @@ const gameboard = (function (playerOne, playerTwo) {
         }
 
     }
+
     function playGame() {
 
         while(true) {
@@ -135,172 +168,20 @@ const gameboard = (function (playerOne, playerTwo) {
         }
     }
 
+    restartBtn.addEventListener("click", () => {
+            board = ["-","-","-","-","-","-","-","-","-"]
+            gameOver = false
+            currentPlayer = playerOne
+            turnDisplay.textContent = ""
+            for(let i = 0; i < cells.length; i++) {
+                cells[i].textContent = ""
+            }
+        })
+
     return {
         playGame,
     }
-})(new playerMaker("playerOne", "X"), new playerMaker("playerTwo", "O"));
+
+})(new playerMaker("playerOne", "X"), new playerMaker("playerTwo", "O"), document);
 
 //gameboard.playGame()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const O_WIN_VALUE = 1;
-const TIE_VALUE = 2
-const X_WIN_VALUE = 3;
-const NOTHING_VALUE = 0;
-
-const playerOne = {
-    name: "playerOne",
-    marker: "X"
-}
-
-const playerTwo = {
-    name: "PlayerTwo",
-    marker: "O"
-}
-
-let currentPlayer = playerOne
-
-
-let board = ["-","-","-","-","-","-","-","-","-"]
-//print Board
-function printBoard() {
-    console.log(`${board[0]} ${board[1]} ${board[2]}`)
-    console.log(`${board[3]} ${board[4]} ${board[5]}`)
-    console.log(`${board[6]} ${board[7]} ${board[8]}`)
-}
-
-//selecton 
-function selection(marker, num) {
-    if(num > 9) {
-        return
-    }
-    let x = num
-    board[x] = marker
-}
-
-//check for winner
-
-function checkWinningCombo (marker) {
-    let emptySpace = 0;
-    let winningCombo = [
-        //winning row
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        //winning column
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        //winning diagonal
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-
-
-    for(let i = 0; i < winningCombo.length; i++) {
-
-        let currentCombination = winningCombo[i] // [0, 1, 2]
-        if(board[currentCombination[0]] == marker && board[currentCombination[1]] == marker && board[currentCombination[2]] == marker){
-            console.log("You win!")
-            if(marker == "X"){
-                return X_WIN_VALUE;
-            }
-            return O_WIN_VALUE
-        }
-    }
-
-    for(let i = 0; i < board.length; i++) {
-        let availableSpots = board[i]
-
-        if(availableSpots == "-") {
-            emptySpace++ 
-        }
-    }
-    if(emptySpace == 0) {
-        console.log("It's a tie")
-        return TIE_VALUE
-    }
-    return NOTHING_VALUE
-}
-
-// take turn
-
-function takeTurn() {
-
-    function selection(marker, num) {
-        if(num > 9) {
-            return
-        }
-        let x = num
-        board[x] = marker
-    }
-    
-    let position;
-
-    if(currentPlayer == playerOne) {
-
-        while (true) {
-            position = prompt("Pick a number 0 - 8")
-            if(position < 9 && position >= 0 && board[position] == "-") {
-                selection(playerOne.marker, position)
-                break
-            }
-            position = "not a valid Number!"
-        }
-
-        currentPlayer = playerTwo;
-        return checkWinningCombo(playerOne.marker)
-        
-    } else {
-
-        let computerChoice = Math.floor(Math.random() * 9);
-
-        while (true) {
-            if(board[computerChoice] == "-") {
-                position = computerChoice
-                selection(playerTwo.marker, position)
-                break
-            }
-            computerChoice = Math.floor(Math.random() * 9);
-        }
-        currentPlayer = playerOne
-        return checkWinningCombo(playerTwo.marker)
-    }
-
-}
-
-
-function playGame() {
-
-    while(true) {
-        printBoard()
-        let result = takeTurn()
-        switch (result) {
-            case X_WIN_VALUE:
-                console.log("X won the game!");
-                return;
-            case O_WIN_VALUE:
-                console.log("O won the game!");
-                return;
-            case TIE_VALUE:
-                console.log("The game was a tie!");
-                return;
-        }
-    }
-}
-
-playGame()
-*/ 
-
